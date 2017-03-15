a296c1df6f3a8217833565c7d82abb709ee17213
Sean Lee <slee12@lpsfo5007199.americas.media.global.loc>
2017-03-14 17:43:12 -0700

    add redux packages to package.json

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import axios from 'axios';
 
 const API_KEY = 'd04457c03f3b4f931f90cfe2fdde3919';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 
 
 class SearchBar extends React.Component{
@@ -11,10 +11,14 @@ class SearchBar extends React.Component{
     super(props);
 
     this.state = {
-      term: ''
+      term: '',
+      city: '',
+      humidity: '',
+      temp: ''
     };
 
-    this.onInputChange=this.onInputChange.bind(this)
+    this.onInputChange=this.onInputChange.bind(this);
+    this.onFormSubmit=this.onFormSubmit.bind(this);
 
   }
 
@@ -23,10 +27,21 @@ class SearchBar extends React.Component{
     this.setState({ term: event.target.value })
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+    const term = this.state.term
+    const url = `${ROOT_URL}&q=${term},us`;
+    const request = axios.get(url);
+    request.then(function(response) {
+      weatherArray = {...response}
+    })
+    console.log(weatherArray);
+  }
+
   render() {
     return(
       <div>
-        <form className="input-group">
+        <form onSubmit={this.onFormSubmit} className="input-group">
           <input
             placeholder="Enter a city"
             className="input-group"
