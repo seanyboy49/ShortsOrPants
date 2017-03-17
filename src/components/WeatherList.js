c621e9e6050dc9b4cfc627fcc07d475801d3bac0
Sean Lee <slee12@lpsfo5007199.americas.media.global.loc>
2017-03-17 15:37:32 -0700

    add logic to display correct suggestion

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -2,34 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends React.Component {
+  renderWeather(cityArray) {
+    if (cityArray.length===0) {
+      return
+    }
+    else {
+      const cityData = cityArray[0];
+      const city = cityData.name;
+      const humidity = cityData.main.humidity;
+      const tempInFarenHeight = cityData.main.temp * 9/5 - 459.67;
+      const formattedTemp = Number((tempInFarenHeight).toFixed(2));
+      const description = cityData.weather[0].description
 
-  renderWeather(cityData) {
-
-    const city = cityData.name;
-    const humidity = cityData.main.humidity;
-    const tempInFarenHeight = cityData.main.temp * 9/5 - 459.67;
-    const formattedTemp = Number((tempInFarenHeight).toFixed(2));
-    const description = cityData.weather[0].description
-
-    return(
-      <div>
-        <h1>{city}</h1>
-        <p>{humidity}</p>
-        <p>{formattedTemp} F</p>
-        <p>{description}</p>
-        {formattedTemp > 50 ? <img src="../img/jeans.png"></img> : <img src="../img/shorts.png"></img>}
-      </div>
-    )
-
+      return(
+        <div>
+          <h1>{city}</h1>
+          <p>{humidity} %</p>
+          <p>{formattedTemp} F</p>
+          <p>{description}</p>
+          {formattedTemp > 50 ?
+            <div>
+              <h2>Wear Jeans!</h2>
+              <img src="../img/jeans.png"></img>
+            </div>
+            :
+            <div>
+              <h2>Wear Shorts!</h2>
+              <img src="../img/shorts.png"></img>
+            </div>}
+        </div>
+      );
+    }
   }
 
-  componentDidUpdate() {
-    console.log(this.props.weather);
-  }
 
   render() {
     return(
-      <div>{this.props.weather.map(this.renderWeather)}</div>
+      <div>{this.renderWeather(this.props.weather)}</div>
     );
   }
 }
